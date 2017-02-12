c837939816ec4055004357fc8001454242e51a26
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 23:07:39 +0900

    fix cache dir ls

--- .test-scripts/save_cache.sh
+++ .test-scripts/save_cache.sh
@@ -8,6 +8,8 @@ then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
 	docker save $(docker history -q docker-mcl3dl:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
 
-	ls -lh ${DOCKER_CACHE_FILE}
+	echo "------------"
+	ls -lh $(dirname ${DOCKER_CACHE_FILE})
+	echo "------------"
 fi
 
