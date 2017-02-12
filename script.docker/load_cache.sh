a27b2b5e1947f0d5c50ed16c77f88aab6f3ff702
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 19:21:48 +0900

    ignore load cache error

--- script.docker/load_cache.sh
+++ script.docker/load_cache.sh
@@ -9,6 +9,6 @@ sudo apt-get install -y liblz4-tool
 echo ${DOCKER_CACHE_FILE}
 
 if [ -f ${DOCKER_CACHE_FILE} ]; then
-	lz4 -dc ${DOCKER_CACHE_FILE} | docker load
+	lz4 -dc ${DOCKER_CACHE_FILE} | docker load || true
 fi
 
