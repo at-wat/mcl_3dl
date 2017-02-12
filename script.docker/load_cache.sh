14201330fc67e522b9b73ceb0df816755afdc021
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 19:14:15 +0900

    test output

--- script.docker/load_cache.sh
+++ script.docker/load_cache.sh
@@ -6,6 +6,8 @@ set -o verbose
 sudo apt-get -qq update
 sudo apt-get install -y liblz4-tool
 
+echo ${DOCKER_CACHE_FILE}
+
 if [ -f ${DOCKER_CACHE_FILE} ]; then
 	lz4 -dc ${DOCKER_CACHE_FILE} | docker load
 fi
