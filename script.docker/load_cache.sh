9d27da3163b2ab9c303004563631baa1c0daabe5
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 18:56:41 +0900

    fix outer apt-get

--- script.docker/load_cache.sh
+++ script.docker/load_cache.sh
@@ -3,7 +3,8 @@
 set -o errexit
 set -o verbose
 
-sudo apt-get install lz4
+sudo apt-get -qq update
+sudo apt-get install -y lz4
 
 if [ -f ${DOCKER_CACHE_FILE} ]; then
 	lz4 -dc ${DOCKER_CACHE_FILE} | docker load
