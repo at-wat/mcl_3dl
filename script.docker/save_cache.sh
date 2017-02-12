e4f782a80ac46ca40c85f49f5d12999b7d0c7ee9
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 19:32:43 +0900

    remove test comment-out

--- script.docker/save_cache.sh
+++ script.docker/save_cache.sh
@@ -3,9 +3,9 @@
 set -o errexit
 set -o verbose
 
-#if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
-#then
+if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
+then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
 	docker save $(docker history -q docker-mcl3dl:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
-#fi
+fi
 
