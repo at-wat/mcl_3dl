555dfd3de76d22db1b78198045ed79b1ec44a811
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-12 19:01:37 +0900

    fix lz4 package name

--- script.docker/load_cache.sh
+++ script.docker/load_cache.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o verbose
 
 sudo apt-get -qq update
-sudo apt-get install -y liblz4-tools
+sudo apt-get install -y liblz4-tool
 
 if [ -f ${DOCKER_CACHE_FILE} ]; then
 	lz4 -dc ${DOCKER_CACHE_FILE} | docker load
