dc89d6507063f3af4794e8c813a22417c13b798d
Atsushi Watanabe <atsushi.w@ieee.org>
2017-02-13 12:37:47 +0900

    fix reflection angle

--- src/mcl_3dl.cpp
+++ src/mcl_3dl.cpp
@@ -622,7 +622,7 @@ private:
 
 								float sin_ang = (d1 - d0) / (inc.norm() * 2.0);
 								// reject total reflection
-								if(sin_ang > params.sin_total_ref)
+								if(sin_ang < params.sin_total_ref)
 								{
 									score_beam *= params.beam_likelihood;
 								}
@@ -899,7 +899,7 @@ public:
 		pub_particle = nh.advertise<geometry_msgs::PoseArray>("particles", 1, true);
 		pub_debug = nh.advertise<sensor_msgs::PointCloud>("debug", 5, true);
 		pub_mapcloud = nh.advertise<sensor_msgs::PointCloud2>("updated_map", 1, true);
-
+ROS_ERROR("Latest");
 		nh.param("map_frame", frame_ids["map"], std::string("map"));
 		nh.param("robot_frame", frame_ids["base_link"], std::string("base_link"));
 		nh.param("odom_frame", frame_ids["odom"], std::string("odom"));
