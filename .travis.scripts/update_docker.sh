60461765afb08e226e714f45f60957d69af139c4
Atsushi Watanabe <atsushi.w@ieee.org>
2017-03-20 17:20:03 +0100

    remove update

--- .travis.scripts/update_docker.sh
+++ .travis.scripts/update_docker.sh
@@ -3,9 +3,6 @@
 set -o errexit
 set -o verbose
 
-sudo apt-get -qq update
-sudo apt-get upgrade -y lxc-docker
-
 echo 'DOCKER_OPTS="-H tcp://127.0.0.1:2375 -H unix:///var/run/docker.sock -s overlay2"' \
 		 | sudo tee /etc/default/docker > /dev/null
 sudo service docker restart
