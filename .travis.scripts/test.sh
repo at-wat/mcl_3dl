1c7f038f3637921dbe0e672a8a5282a85ee1ef0f
Atsushi Watanabe <atsushi.w@ieee.org>
2017-03-21 17:49:41 +0900

    adds test result bot

--- .travis.scripts/test.sh
+++ .travis.scripts/test.sh
@@ -3,6 +3,16 @@
 set -o errexit
 set -o verbose
 
+function post_error()
+{
+	if [[ ${TRAVIS_PULL_REQUEST} == "true" ]];
+	then
+		curl -X POST -H 'Content-Type:application/json' -d "{\"body\":\"** Travis-CI status notifier\n\n$1\"}" \
+			https://api.github.com/repos/${TRAVIS_PULL_REQUEST_SLUG}/pull/${TRAVIS_PULL_REQUEST}/comments?access_token=${TRAVIS_BOT_GITHUB_TOKEN}
+	fi
+}
+
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source /catkin_ws/devel/setup.bash
 
@@ -17,11 +27,13 @@ rosdep install --from-paths src/mcl_3dl --ignore-src --rosdistro=${ROS_DISTRO} -
 apt-get clean && \
 rm -rf /var/lib/apt/lists/*
 
-catkin_make
-catkin_make tests --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON
-catkin_make run_tests  --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON
+catkin_make || post_error('```catkin_make``` failed') || false
+catkin_make tests --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON || post_error('```catkin_make tests``` failed') || false
+catkin_make run_tests  --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON || post_error('```catkin_make run_tests``` failed') || false
 
-catkin_test_results
+catkin_test_results || post_error('Test failed') || false
+
+post_error('All tests passed')
 
 cd ..
 rm -rf /catkin_ws || true
