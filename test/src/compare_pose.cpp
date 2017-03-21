6c6acad068dc981e2a799153c77ba85c23d8a693
Atsushi Watanabe <atsushi.w@ieee.org>
2017-03-22 01:39:18 +0900

    fix compare tf

--- test/src/compare_pose.cpp
+++ test/src/compare_pose.cpp
@@ -1,74 +1,13 @@
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-#include <tf/transform_listener.h>
 
 #include <gtest/gtest.h>
-
-TEST(compare_pose, compare_tf)
-{
-	ros::NodeHandle nh("~");
-	tf::TransformListener tfl;
-
-	size_t cnt = 0;
-	const size_t cnt_max = 4;
-	size_t tf_ex_cnt = 0;
-
-	bool shutdown(false);
-
-	const boost::function<void(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr&)> cb_pose = 
-		[&](const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg)->void{
-			geometry_msgs::PoseStamped pose;
-			try
-			{
-				geometry_msgs::PoseStamped pose_bl;
-				pose_bl.header.frame_id = "base_link";
-				pose_bl.header.stamp = msg->header.stamp;
-				pose_bl.pose.orientation.w = 1.0;
-				tfl.waitForTransform("map", pose_bl.header.frame_id, 
-						pose_bl.header.stamp, ros::Duration(0.1));
-				tfl.transformPose("map", pose_bl, pose);
-			}
-			catch(tf::TransformException &e)
-			{
-				tf_ex_cnt ++;
-				return;
-			}
-			float x_error = pose.pose.position.x - msg->pose.pose.position.x;
-			float y_error = pose.pose.position.y - msg->pose.pose.position.y;
-			float z_error = pose.pose.position.z - msg->pose.pose.position.z;
-			float error = sqrtf(powf(x_error, 2.0) + powf(y_error, 2.0) + powf(z_error, 2.0));
-
-			fprintf(stderr, "compare_tf[%d/%d]:\n",
-					(int)cnt, (int)cnt_max);
-			fprintf(stderr, "  error=%0.3f\n", error);
-
-			cnt ++;
-			if(cnt >= cnt_max) shutdown = true;
-
-			ASSERT_FALSE(error > 0.01)
-				<< "tf output diverges from amcl_pose.";
-		};
-	ros::Subscriber sub_pose = nh.subscribe("/amcl_pose", 1, cb_pose);
-	
-	ros::Rate wait(1);
-
-	while(ros::ok() && !shutdown)
-	{
-		ros::spinOnce();
-		wait.sleep();
-	}
-	
-	ASSERT_FALSE(tf_ex_cnt > 1)
-		<< "tf exception occures more than once.";
-}
 
 TEST(compare_pose, compare)
 {
 	ros::NodeHandle nh("~");
 	
-	bool shutdown(false);
-
 	nav_msgs::Path path;
 	size_t i_path;
 
@@ -101,7 +40,7 @@ TEST(compare_pose, compare)
 				fprintf(stderr, "  z error/3sigma=%0.3f/%0.3f\n", z_error, z_sigma * 3.0);
 
 				i_path ++;
-				if(i_path >= path.poses.size()) shutdown = true;
+				if(i_path >= path.poses.size()) ros::shutdown();
 
 				ASSERT_FALSE(error > error_limit)
 					<< "Position error is larger then expected.";
@@ -119,11 +58,12 @@ TEST(compare_pose, compare)
 
 	ros::Rate wait(10);
 
-	while(ros::ok() && !shutdown)
+	while(ros::ok())
 	{
 		ros::spinOnce();
 		wait.sleep();
 	}
+	fprintf(stderr, "compare_pose finished\n");
 }
 
 
