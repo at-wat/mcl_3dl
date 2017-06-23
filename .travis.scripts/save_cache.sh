f51b13bf68d4f1300a12644be04840e7c7de9dc4
Atsushi Watanabe <atsushi.w@ieee.org>
2017-06-23 13:44:00 +0900

    Use smaller docker image (for test).

--- .travis.scripts/save_cache.sh
+++ .travis.scripts/save_cache.sh
@@ -8,8 +8,9 @@ then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
 	docker save $(docker history -q docker-mcl3dl:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
 
-	echo "------------"
-	ls -lh $(dirname ${DOCKER_CACHE_FILE})
-	echo "------------"
 fi
+
+echo "------------"
+ls -lh $(dirname ${DOCKER_CACHE_FILE})
+echo "------------"
 
