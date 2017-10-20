de5a8f439c20669989885ee359900141a081ac9a
Atsushi Watanabe <atsushi.w@ieee.org>
2017-10-20 12:04:44 +0900

    Fix build on indigo. (#84)

--- .travis.scripts/save_cache.sh
+++ .travis.scripts/save_cache.sh
@@ -6,7 +6,7 @@ set -o verbose
 if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
 then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
-	docker save $(docker history -q docker-mcl3dl:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
+	docker save $(echo ${DOCKER_CACHE_TARGETS} | xargs -n1 docker history -q | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
 
 	echo "------------"
 	ls -lh $(dirname ${DOCKER_CACHE_FILE})
--- .travis.scripts/test.sh
+++ .travis.scripts/test.sh
@@ -15,15 +15,19 @@ mkdir -p /catkin_ws/build/mcl_3dl/test/
 mv /catkin_ws/src/mcl_3dl/.cached-dataset/* /catkin_ws/build/mcl_3dl/test/
 ls -lh /catkin_ws/build/mcl_3dl/test/
 
-apt-get -qq update && \
-apt-get install libxml2-utils && \
-rosdep install --from-paths src/mcl_3dl --ignore-src --rosdistro=${ROS_DISTRO} -y && \
-apt-get clean && \
-rm -rf /var/lib/apt/lists/*
+CATKIN_OPTIONS=""
+if [ x${ROS_DISTRO} == "xindigo" ]
+then
+  CATKIN_OPTIONS="-DCMAKE_BUILD_TYPE=Release"
+  echo "On indigo-trusty, we need release build due to the bug of PCL1.7 with c++11." 1>&2
+fi
 
-catkin_make || (gh-pr-comment FAILED '```catkin_make``` failed'; false)
-catkin_make tests --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON || (gh-pr-comment FAILED '```catkin_make tests``` failed'; false)
-catkin_make run_tests  --cmake-args -DMCL_3DL_EXTRA_TESTS:=ON || (gh-pr-comment FAILED '```catkin_make run_tests``` failed'; false)
+catkin_make ${CATKIN_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+catkin_make tests --cmake-args -DMCL_3DL_EXTRA_TESTS=ON ${CATKIN_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+catkin_make run_tests --cmake-args -DMCL_3DL_EXTRA_TESTS=ON ${CATKIN_OPTIONS} || \
+  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ catkin_test_results ];
 then
@@ -40,9 +44,9 @@ else
 `find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
 "
 fi
-catkin_test_results || (gh-pr-comment FAILED "Test failed$result_text"; false)
+catkin_test_results || (gh-pr-comment "FAILED on ${ROS_DISTRO}" "Test failed$result_text"; false)
 
-gh-pr-comment PASSED "All tests passed$result_text"
+gh-pr-comment "PASSED on ${ROS_DISTRO}" "All tests passed$result_text"
 
 cd ..
 rm -rf /catkin_ws || true
