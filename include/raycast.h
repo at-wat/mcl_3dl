b3aed0d0f1e537025a47a26015620437c2fb03db
Atsushi Watanabe <atsushi.w@ieee.org>
2018-02-05 19:51:28 +0900

    Visualize sampled points and raycasting result (#97)
    * Visualize sampled points and raycasting result
    
    * Remove duplicated code around raycasting

---
+++ include/raycast.h
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2016-2017, the mcl_3dl authors
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its 
+ *       contributors may be used to endorse or promote products derived from 
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RAYCAST_H
+#define RAYCAST_H
+
+#include <vec3.h>
+
+#include <vector>
+
+class Raycast
+{
+public:
+  class CastResult
+  {
+  public:
+    Vec3 pos_;
+    bool collision_;
+    float sin_angle_;
+
+    CastResult(const Vec3 pos, bool collision, float sin_angle)
+      : pos_(pos)
+      , collision_(collision)
+      , sin_angle_(sin_angle)
+    {
+    }
+  };
+  class Iterator
+  {
+  protected:
+    pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree_;
+    Vec3 pos_;
+    Vec3 inc_;
+    size_t count_;
+    size_t length_;
+    float grid_;
+
+  public:
+    friend Raycast;
+
+    Iterator(
+        pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree,
+        const Vec3 begin, const Vec3 end, const float grid)
+    {
+      kdtree_ = kdtree;
+      length_ = (end - begin).norm() / grid - 1;
+      inc_ = (end - begin) / length_;
+      pos_ = begin + inc_;
+      count_ = 1;
+      grid_ = grid;
+    }
+    Iterator &operator++()
+    {
+      ++count_;
+      pos_ += inc_;
+      return *this;
+    }
+    CastResult operator*()
+    {
+      bool collision(false);
+      float sin_ang(0.0);
+
+      pcl::PointXYZI center;
+      center.x = pos_.x;
+      center.y = pos_.y;
+      center.z = pos_.z;
+      center.intensity = 0.0;
+      std::vector<int> id(1);
+      std::vector<float> sqdist(1);
+      if (kdtree_->radiusSearch(
+              center,
+              grid_ / 2.0, id, sqdist, 1))
+      {
+        collision = true;
+
+        const float d0 = sqrtf(sqdist[0]);
+        const Vec3 pos_prev = pos_ - (inc_ * 2.0);
+        pcl::PointXYZI center_prev;
+        center_prev.x = pos_prev.x;
+        center_prev.y = pos_prev.y;
+        center_prev.z = pos_prev.z;
+        center_prev.intensity = 0.0;
+        kdtree_->nearestKSearch(center_prev, 1, id, sqdist);
+        float d1 = sqrtf(sqdist[0]);
+
+        sin_ang = (d1 - d0) / (inc_.norm() * 2.0);
+        if (fabs(d1 - d0) < 1e-6)
+        {
+          sin_ang = M_PI;
+        }
+      }
+      return CastResult(pos_, collision, sin_ang);
+    }
+    bool operator!=(const Iterator &a) const
+    {
+      return count_ != a.count_;
+    }
+  };
+
+protected:
+  pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree_;
+  Iterator begin_;
+  Iterator end_;
+
+public:
+  Raycast(
+      pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr kdtree,
+      const Vec3 begin, const Vec3 end, const float grid)
+    : begin_(kdtree, begin, end, grid)
+    , end_(kdtree, begin, end, grid)
+  {
+    kdtree_ = kdtree;
+    end_.count_ = end_.length_;
+  }
+  Iterator begin()
+  {
+    return begin_;
+  }
+  Iterator end()
+  {
+    return end_;
+  }
+};
+
+#endif  // RAYCAST_H
