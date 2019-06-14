1f85160fd0dadf68be5d9814353ffc95ee925aa1
Naotaka Hatao <hatao.jsk@gmail.com>
2019-06-14 15:53:25 +0900

    Fix ParticleFilter::resample()
    * Fix resample() to remove first particle correctly.
    * A test is added to verify this fix.

--- include/mcl_3dl/pf.h
+++ include/mcl_3dl/pf.h
@@ -35,6 +35,7 @@
 #include <algorithm>
 #include <functional>
 #include <cmath>
+#include <limits>
 
 namespace mcl_3dl
 {
@@ -155,10 +156,10 @@ template <typename T, typename FLT_TYPE = float, typename MEAN = ParticleWeighte
 class ParticleFilter
 {
 public:
-  explicit ParticleFilter(const int nParticles)
+  explicit ParticleFilter(const int num_particles)
     : engine_(seed_gen_())
   {
-    particles_.resize(nParticles);
+    particles_.resize(num_particles);
   }
   void init(T mean, T sigma)
   {
@@ -180,16 +181,16 @@ public:
     particles_dup_ = particles_;
     std::sort(particles_dup_.begin(), particles_dup_.end());
 
-    const FLT_TYPE pstep = accum / (particles_.size() - 1);
-    FLT_TYPE pscan = 0;
+    const FLT_TYPE pstep = accum / particles_.size();
+    static const FLT_TYPE EPS = std::numeric_limits<float>::epsilon();
     auto it = particles_dup_.begin();
     auto it_prev = particles_dup_.begin();
-
     const FLT_TYPE prob = 1.0 / particles_.size();
-    for (auto& p : particles_)
+    for (size_t i = 0; i < particles_.size(); ++i)
     {
+      auto &p = particles_[i];
+      const FLT_TYPE pscan = pstep * (i + 1) - EPS;
       it = std::lower_bound(it, particles_dup_.end(), Particle<T, FLT_TYPE>(pscan));
-      pscan += pstep;
       p.probability_ = prob;
       if (it == particles_dup_.end())
       {
