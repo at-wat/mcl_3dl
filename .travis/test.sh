9dc5eb95ca4c6288314b1b80fb17db475ff84b76
Atsushi Watanabe <atsushi.w@ieee.org>
2019-07-07 14:58:42 +0900

    Fix bot comment

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,10 +5,11 @@ set -o errexit
 source /opt/ros/${ROS_DISTRO}/setup.bash
 cd /catkin_ws
 
+build_number="[[#${TRAVIS_BUILD_NUMBER}](${TRAVIS_BUILD_WEB_URL})]"
 
 pkgs=$(find . -name package.xml | xargs -n1 dirname)
 catkin_lint $pkgs \
-  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" \
+  || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" \
       "<details><summary>catkin_lint failed</summary>
 
 \`\`\`
@@ -28,11 +29,11 @@ sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror -O1 -coverage")' \
 CM_OPTIONS=''
 
 catkin_make -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
-  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+  (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
 catkin_make tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
-  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+  (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
 catkin_make run_tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
-  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
+  (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ catkin_test_results ];
 then
@@ -49,12 +50,12 @@ $(catkin_test_results --all | grep -v Skipping || true)
 $(find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;')
 "
 fi
-catkin_test_results || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
+catkin_test_results || (gh-pr-comment "${build_number} FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
 
 $result_text</details>"; false)
 
 (cd src/mcl_3dl/; cp -r /catkin_ws/build ./; bash <(curl -s https://codecov.io/bash) -y .codecov.yml)
 
-gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
+gh-pr-comment "${build_number} PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
 
 $result_text</details>" || true
