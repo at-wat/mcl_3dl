94eeb004b077e8236e0fb42d451c766ba258da03
Atsushi Watanabe <atsushi.w@ieee.org>
2020-05-05 05:55:52 +0900

    Retry gpg keyserver on prerelease test (#312)

--- .travis/prerelease_test.sh
+++ .travis/prerelease_test.sh
@@ -24,7 +24,11 @@ case ${ROS_DISTRO_TARGET} in
 esac
 
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/ros-latest.list
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+for i in 1 2 3; do
+  sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 \
+    && break \
+    || true
+done
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends python3-ros-buildfarm python3-pip git
 sudo pip3 install git+https://github.com/ros/catkin
