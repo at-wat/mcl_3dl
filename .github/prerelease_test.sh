2d3c2253cbbd0b1fda256f6416a766df9ba92497
Atsushi Watanabe <atsushi.w@ieee.org>
2020-11-04 18:08:49 +0900

    Fix branch used in prerelease test

--- .github/prerelease_test.sh
+++ .github/prerelease_test.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+source_ref=$1
+
 case ${ROS_DISTRO} in
   kinetic )
     UBUNTU_DIST_TARGET=xenial
@@ -25,8 +27,12 @@ for i in 1 2 3; do
     || true
 done
 sudo apt-get update -qq
-sudo apt-get install -y --no-install-recommends python3-ros-buildfarm python3-pip git
-sudo pip3 install git+https://github.com/ros/catkin
+sudo apt-get install -y --no-install-recommends \
+  git \
+  jq \
+  python3-pip \
+  python3-ros-buildfarm
+sudo python3 -m pip install git+https://github.com/ros/catkin
 
 mkdir -p /tmp/prerelease_job
 cd /tmp/prerelease_job
@@ -37,17 +43,16 @@ git clone \
   -b apt-get-us-east-1 \
   https://github.com/at-wat/ros_buildfarm.git ros_buildfarm
 
-sudo pip3 install ./ros_buildfarm
+sudo python3 -m pip install ./ros_buildfarm
 
 
-git_ref=$(echo ${GITHUB_REF} | sed 's|^refs/\S\+/||')
 build_link="[${GITHUB_RUN_ID}-prerelease]"
 
 generate_prerelease_script.py \
   https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml \
   ${ROS_DISTRO} default ubuntu ${UBUNTU_DIST_TARGET} amd64 \
   --custom-repo \
-    mcl_3dl__custom-2:git:https://github.com/at-wat/mcl_3dl.git:${git_ref} \
+    mcl_3dl__custom-2:git:https://github.com/at-wat/mcl_3dl.git:${source_ref} \
     mcl_3dl_msgs__custom-2:git:https://github.com/at-wat/mcl_3dl_msgs.git:master \
   --level 1 \
   --output-dir ./
