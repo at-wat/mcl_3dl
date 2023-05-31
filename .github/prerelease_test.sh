ff932f5ec33abe6e287eaa24a94bc098736330db
Atsushi Watanabe <atsushi.w@ieee.org>
2023-05-31 14:20:49 +0900

    Update assets to v0.5.2 (#405)

--- .github/prerelease_test.sh
+++ .github/prerelease_test.sh
@@ -18,31 +18,25 @@ case ${ROS_DISTRO} in
     ;;
 esac
 
-echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/ros-latest.list
-for i in 1 2 3; do
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 \
-    && break \
-    || true
-  sleep 5
-done
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
   git \
   jq \
-  python3-pip \
-  python3-ros-buildfarm
-sudo python3 -m pip install git+https://github.com/ros/catkin
+  python3-pip
+python3 -m pip install --upgrade \
+  pip \
+  setuptools \
+  wheel
+python3 -m pip install \
+  git+https://github.com/at-wat/ros_buildfarm.git@apt-get-us-east-1
 
 mkdir -p /tmp/prerelease_job
 cd /tmp/prerelease_job
-
 
 git clone \
   --depth 1 \
   -b apt-get-us-east-1 \
   https://github.com/at-wat/ros_buildfarm.git ros_buildfarm
-
-sudo python3 -m pip install ./ros_buildfarm
 
 
 build_link="[${GITHUB_RUN_NUMBER}-prerelease]"
